127529e52dd9a31f51d018a63c5fe07966e260fc
Humpty Dumpty <zhanibek220506@gmail.com>
2025-04-04 20:41:08 +0500

    added models and serializers

--- back/api/urls.py
+++ back/api/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProfileViewSet, SwipeViewSet, ChatViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'profiles', ProfileViewSet)
+router.register(r'swipes', SwipeViewSet)
+router.register(r'chats', ChatViewSet)
+router.register(r'messages', MessageViewSet)
 
 urlpatterns = [
-
+    path('api/', include(router.urls)),
 ]
+
